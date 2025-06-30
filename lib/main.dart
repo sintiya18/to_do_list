7977883b123e078827fcf25d663b00dc8893c3e8
Fito <rizkyfito28@gmail.com>
2025-06-30 12:51:25 +0700

    ganti home

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: RegisterScreen(),
+      home: DaftarTugasScreen(),
     );
   }
 }
