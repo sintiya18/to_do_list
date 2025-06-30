d4f1df6657a2174e77095ef83e4a977076b98f50
Fito <rizkyfito28@gmail.com>
2025-06-30 11:15:59 +0700

    hapus main

--- lib/tambah_tugas_screen.dart
+++ lib/tambah_tugas_screen.dart
@@ -1,19 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: DaftarTugasScreen(),
-    );
-  }
-}
-
 class DaftarTugasScreen extends StatelessWidget {
   const DaftarTugasScreen({super.key});
 
