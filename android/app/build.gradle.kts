da9699e07a15da5ea103f9b7541415f0225609cf
tiyaasintiya18 <tiyaasintiya18@gmail.com>
2025-07-07 05:18:52 +0000

    setting playstore

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,7 @@
+// Import diletakkan di bagian paling atas file.
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -5,8 +9,15 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+// Logika untuk memuat file properties diletakkan setelah blok plugins.
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 android {
-    namespace = "com.example.to_do_list"
+    namespace = "com.PlanInApp"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +32,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.to_do_list"
+        applicationId = "com.PlanInApp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -30,13 +41,28 @@ android {
         versionName = flutter.versionName
     }
 
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+    // --- BLOK YANG DIPERBAIKI ---
+    signingConfigs {
+        // Gunakan create("release") untuk membuat konfigurasi baru di KTS
+        create("release") {
+            if (keystorePropertiesFile.exists()) {
+                // Gunakan '=' untuk assignment dan getProperty("...") untuk mengambil nilai
+                storeFile = file(keystoreProperties.getProperty("storeFile"))
+                storePassword = keystoreProperties.getProperty("storePassword")
+                keyAlias = keystoreProperties.getProperty("keyAlias")
+                keyPassword = keystoreProperties.getProperty("keyPassword")
+            }
         }
     }
+
+    buildTypes {
+        // Gunakan getByName("release") untuk mengkonfigurasi build type yang sudah ada
+        getByName("release") {
+            // Mengatur agar build type 'release' menggunakan konfigurasi 'release' yang telah dibuat di atas.
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+    // --- AKHIR BLOK YANG DIPERBAIKI ---
 }
 
 flutter {
--- android/app/src/main/kotlin/com/PlanInApp/MainActivity.kt
+++ android/app/src/main/kotlin/com/PlanInApp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.to_do_list
+package com.PlanInApp
 
 import io.flutter.embedding.android.FlutterActivity
 
