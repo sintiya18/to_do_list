7c538991b916bb6761abc36e3a4b588152348f7f
Fito <rizkyfito28@gmail.com>
2025-07-21 12:23:54 +0700

    run

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-     
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: SplashScreen()
+      home: DaftarTugasScreen()
     );
   }
 }
